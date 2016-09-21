5a63c6f28f879f1fee8718dc39ee011c6405401b
Rob Pataki <rob@handcrafteddigital.london>
2016-09-21 15:30:41 +0100

    Finalise Countdown component
    - Add page title
    - Add tests for pause and clear buttons

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -33,13 +33,36 @@ describe('Countdown', () => {
       expect(countdown.state.count).toBe(2);
       expect(countdown.state.countdownStatus).toBe('started');
 
-      done();
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 2001);
+    });
 
-      // setTimeout(() => {
-      //   expect(countdown.state.count).toBe(0);
-      //   expect(countdown.state.countdownStatus).toBe('stopped');
-      //   done();
-      // }, 2001);
+    it('should pause countdown on `paused` status', (done) =>{
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should stop countdown on `stopped` status', (done) =>{
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
   });
 });
